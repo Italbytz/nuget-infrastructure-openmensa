14815f64cd829ecd813138fe589669a17ea3deb7
Robin Nunkesser <robin.nunkesser@hshl.de>
2021-11-09 13:36:08 +0100

    Initial project

---
+++ Italbytz.Infrastructure.OpenMensa/Italbytz.Infrastructure.OpenMensa.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Italbytz.Infrastructure.OpenMensa.Tests
+{
+    public class Tests
+    {
+        OpenMensaAPI api;
+
+        [SetUp]
+        public void Setup()
+        {
+            api = new OpenMensaAPI();
+        }
+
+        [Test]
+        public async void Test1()
+        {
+            var canteens = await api.RetrieveCanteens();
+            Assert.Pass();
+        }
+    }
+}
---
+++ Italbytz.Infrastructure.OpenMensa/Italbytz.Infrastructure.OpenMensa/OpenMensaAPI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Italbytz.Infrastructure.OpenMensa
+{
+    public class OpenMensaAPI
+    {
+        private const string MediaTypeJSON = "application/json";
+
+        private static readonly HttpClient HttpClient;
+
+        static OpenMensaAPI()
+        {
+            HttpClient = new HttpClient()
+            {
+                BaseAddress = new Uri("https://openmensa.org/api/v2")
+            };
+            var media = new MediaTypeWithQualityHeaderValue(MediaTypeJSON);
+            HttpClient.DefaultRequestHeaders.Accept.Add(media);
+        }
+
+        public async Task<List<Canteen>> RetrieveCanteens()
+        {
+            return await HttpClient.GetFromJsonAsync<List<Canteen>>("/canteens");
+        }
+
+    }
+}
+
